750a201eb296e276d8039edbed712bc4e4148b18
JamesH <jwhusc7@gmail.com>
2022-06-08 13:46:53 -0400

    added code snapshot

---
+++ assets/js/event.js
@@ -0,0 +1,7 @@
+if (window.location.href.indexOf("event") > -1) {
+  const currentEvent = JSON.parse(localStorage.getItem("currentEvent")) || {
+      title: "Title Placeholder",
+      subtitle: "",
+      description: ""
+  }
+};
